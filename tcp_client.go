c6bb7524e5b41431d787146accbd26616134d3d1
erny03 <erikn@uia.no>
2023-05-04 21:04:45 +0000

    final?

--- tcp_client.go
+++ tcp_client.go
@@ -12,10 +12,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-    
+
 	log.Println("os.Args[1] = ", os.Args[1])
 
+
 	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+
 	log.Println("Kryptert melding: ", string(kryptertMelding))
 
 	_, err = conn.Write([]byte(string(kryptertMelding)))
@@ -27,7 +29,12 @@ func main() {
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
-	} 
+	}
+
 	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+
+	dekryptertMelding := mycrypt.Krypter([]rune(response), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+
+	log.Printf("reply from proxy: %s", string(dekryptertMelding))
+
 }
